4478e6b491e3ef983658248b3e2afba58a9096b5
ashik <ashikurrahman7134@gmail.com>
2025-05-05 21:37:58 +0600

    use mvc pattern

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('/', 'home')->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::view('/contact', 'contact')->name('contact');
 Route::view('/about', 'about')->name('about');
+Route::view('/article', 'article')->name('article');
