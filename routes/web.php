2644f2481cbacff534acf344a81b5cc84d143060
ashik <ashikurrahman7134@gmail.com>
2025-05-05 16:41:29 +0600

    added name route

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('/', 'home');
+Route::view('/', 'home')->name('home');
+Route::view('/contact', 'contact')->name('contact');
+Route::view('/about', 'about')->name('about');
