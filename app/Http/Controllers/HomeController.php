e28df0a9bbe322cf1c355c2ac08f6c25b08e104a
ashik <ashikurrahman7134@gmail.com>
2025-05-05 22:13:32 +0600

    dynamic data add

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
         $categories = Category::get();
-        return view('home', compact('categories'));
+        $posts = Post::when(request('category_id'), function($query){
+            $query->where('category_id', request('category_id'));
+        })
+        ->latest()
+        ->get();
+        return view('home', compact('categories', 'posts'));
     }
 }
