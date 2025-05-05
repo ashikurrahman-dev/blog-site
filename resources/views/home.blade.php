4ad2b87f92aab3031fe4974e43d3fa1bf424859e
ashik <ashikurrahman7134@gmail.com>
2025-05-05 22:40:40 +0600

    view blog post

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,10 @@
                         <img src="{{ asset('images/placeholder-150x150.png') }}" alt="Post Image"
                             class="w-32 h-32 object-cover rounded">
                         <div>
-                            <h3 class="text-lg font-semibold"><a href="#" class="hover:underline">{{ $post->title }}</a>
+                            <h3 class="text-lg font-semibold">
+                                <a href="{{ route('post.show', $post) }}" class="hover:underline">
+                                    {{ $post->title }}
+                                </a>
                             </h3>
                             <p class="text-gray-600">{{ substr($post->text, 0, 200) }} .....</p>
                         </div>
